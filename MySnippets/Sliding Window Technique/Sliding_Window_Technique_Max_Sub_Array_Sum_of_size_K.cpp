ca1e535556cc67207086488cf475d3bee1fa74b5
singhisking123 <singhiskingdeepankar@gmail.com>
2022-08-11 18:39:48 +0530

    our first Commit of Codes

--- MySnippets/Sliding Window Technique/Sliding_Window_Technique_Max_Sub_Array_Sum_of_size_K.cpp
+++ MySnippets/Sliding Window Technique/Sliding_Window_Technique_Max_Sub_Array_Sum_of_size_K.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+// array --> 2, 5, 1, 8, 2, 9, 1
+// k --> 3
 
+int solve (vector<int> & arr)
+{
 
+	int i = 0, j = 0, mx = INT_MIN, n = arr.size();
+	int sum = 0;
+	while (j < n)
+	{
+		sum = sum + arr[j];
+		if (j - i + 1  < k) {
+			j++;
+		}
+		else if (j - i + 1 == k) {
+			mx = max(mx, sum);
+			i++; j++;
+			sum = sum - arr[i - 1];
+		}
+	}
+}
 
 int main()
 {
